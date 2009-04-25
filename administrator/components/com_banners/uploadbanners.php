2852b28f3e48a485b1d96cf8982dddd2e964c7a9
megazaisl <megazaisl@3b7f9ca5-0b46-0410-b0b7-7d73c3805494>
2009-04-25 05:37:28 +0000

    Изменено место расположения языковых файлов для компонента com_banners.php. Раньше языки располагались в components/com_banners/language. Теперь - в language/russian/com_banners.php. Из общего языкового файла language/russian.php блоки для этого компонента перенесены в новый языковой файл. В файлах компонента внесены соответствующие правки (в местах подключения языка)

--- administrator/components/com_banners/uploadbanners.php
+++ administrator/components/com_banners/uploadbanners.php
@@ -11,7 +11,13 @@ define("_VALID_MOS", 1);
 
 /** security check */
 require ('../../includes/auth.php');
-include_once ($mosConfig_absolute_path . '/language/' . $mosConfig_lang . '.php');
+if(file_exists($mosConfig_absolute_path . '/language/'.$mosConfig_lang.'/com_banners.php')) {
+	$artbannerslanguage = $mosConfig_lang;
+}else{
+	$artbannerslanguage = 'russian';
+}
+
+include_once ($mosConfig_absolute_path . '/language/'.$artbannerslanguage.'/com_banners.php');
 
 // limit access to functionality
 $option = strval(mosGetParam($_SESSION, 'option', ''));
