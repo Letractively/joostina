631a66d4770fb56f0021e45a638e16ce4e97ddea
megazaisl <megazaisl@3b7f9ca5-0b46-0410-b0b7-7d73c3805494>
2009-05-17 11:25:40 +0000

    Правки. Для использования шаблонов нужно в БД выполнить ALTER TABLE `jos_sections` ADD `templates` TEXT NOT NULL ; ALTER TABLE `jos_categories` ADD `templates` TEXT NOT NULL ; ALTER TABLE `jos_content` ADD `templates` VARCHAR( 255 ) NOT NULL ;

--- components/com_content/view/item/static/default.php
+++ components/com_content/view/item/static/default.php
@@ -1,7 +1,6 @@
 <?php
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 ?>
-        item_static
 		<?php if($params->get('item_title')) { ?>
 
             <div <?php echo $news_uid_css_title; ?>class="item_title">
