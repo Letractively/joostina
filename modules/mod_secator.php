89ed36dd16c4ee5266bb93b30a04627c01fed7fa
bost56 <bost56@3b7f9ca5-0b46-0410-b0b7-7d73c3805494>
2009-06-24 10:55:17 +0000

    Небольшие доработки модуля вывода списка категорий/разделов.

--- modules/mod_secator.php
+++ modules/mod_secator.php
@@ -12,75 +12,93 @@ defined( '_VALID_MOS' ) or die();
 
 $use_to = intval($params->get( 'use_to', 0 ));
 
+
+if(!defined('_SECATOR_INCLUDE')){
+	DEFINE('_SECATOR_INCLUDE',1);
+
+	class mod_secator{
+
+		# используем для разделов
+		function use_sections(&$params){
+
+			$view_sections	= strval($params->get( 'view_section', 'section_defaults.php' ));
+
+			$config			= &Jconfig::getInstance();
+			$file			= $config->config_absolute_path.DS.'modules'.DS.'mod_secator'.DS.'section'.DS.$view_sections;
+			$file_defaults	= $config->config_absolute_path.DS.'modules'.DS.'mod_secator'.DS.'section'.DS.'section_defaults.php';
+
+			require is_file($file) ? $file : $file_defaults;
+		}
+
+		# используем для категорий
+		function use_category(&$params){
+
+			$view_category	= strval($params->get( 'view_category', 'category_defaults.php' ));
+
+			$config			= &Jconfig::getInstance();
+			$file			= $config->config_absolute_path.DS.'modules'.DS.'mod_secator'.DS.'category'.DS.$view_category;
+			$file_defaults	= $config->config_absolute_path.DS.'modules'.DS.'mod_secator'.DS.'category'.DS.'category_defaults.php';
+
+			require is_file($file) ? $file : $file_defaults;
+		}
+
+
+		/* получение типа публикации */
+		function get_where_published($show_published=null,$name='published'){
+
+			if($show_published==0){
+				$_where_published = $name.'=0';
+			}elseif($show_published==2){
+				$_where_published = ' ';
+			}else{
+				$_where_published = $name.'=1';
+			}
+			return $_where_published;
+		}
+
+
+		function get_secator_link($row,$params){
+			$link = 'index.php?option=com_content';
+
+			$Itemid = $params->get( 'Itemid', null );
+			$use_to = $params->get( 'use_to', 0 );
+
+			if($use_to==1){
+				// используем категроии
+				$task = $params->get( 'task_category', 'blogcategory' );
+			}else{
+				$task = $params->get( 'task_category', 'blogsection' );
+			}
+
+			$link = $link.'&task='.$task.'&id='.$row->id.'&Itemid='.$Itemid;
+
+			return sefRelToAbs($link);
+		}
+
+		/* подсчет числа содержимого в категории */
+		function get_count_content(){
+			static $return;
+
+			if(!$return){
+				$db = &database::getInstance();
+				$sql = 'SELECT catid,count(id) as count FROM #__content GROUP BY catid';
+				$db->setQuery($sql);
+				$cats = $db->loadObjectList();
+				$ret = array();
+				foreach($cats as $cat){
+					$ret[$cat->catid]=$cat->count;
+				}
+				$return = $ret;
+			}
+			return $return;
+		}
+	}
+}
+
 if($use_to==1){
-	_use_category(&$params); // используем категроии
+	mod_secator::use_category($params); // используем категроии
 }else{
-	_use_sections(&$params); // используем разделы
-}
-
-
-# используем для разделов
-function _use_sections(&$params){
-	$order_section = strval($params->get( 'order_section', 'order' ));
-}
-
-# используем для категорий
-function _use_category(&$params){
-	$database = &database::getInstance();
-
-	$count			= intval($params->get( 'count', 5 ));
-	$order_asc_desc	= strval($params->get( 'order_asc_desc', 'ASC' ));
-	$order_category	= strval($params->get( 'order_category', 'order' ));
-
-	$_where = 'WHERE ';
-	$_where_published = _get_where_published($params->get( 'show_published', 1 ));
-
-	$sql = 'SELECT #__categories.id,#__categories.title,#__categories.name,#__categories.image,#__categories.image_position,#__categories.description from #__categories
-			INNER JOIN #__sections ON #__sections.id = #__categories.section
-			'.$_where.$_where_published.' ORDER BY #__categories.'.$order_category.' '.$order_asc_desc.' ';
-	$database->setQuery( $sql,0,$count );
-	$database->getQuery( );
-	$rows = $database->loadObjectList();
-
-	$view_category	= strval($params->get( 'view_category', 'category_defaults.php' ));
-
-	$_config		= Jconfig::getInstance();
-	$file			= $_config->config_absolute_path.DS.'modules'.DS.'mod_secator'.DS.$view_category;
-	$file_defaults	= $_config->config_absolute_path.DS.'modules'.DS.'mod_secator'.DS.'category_defaults.php';
-
-	require_once is_file($file) ? $file : $file_defaults;
-}
-
-/* получение типа публикации */
-function _get_where_published($show_published=null){
-
-	if($show_published==0){
-		$_where_published = ' #__categories.published=0';
-	}elseif($show_published==2){
-		$_where_published = ' ';
-	}else{
-		$_where_published = ' #__categories.published=1';
-	}
-	return $_where_published;
-}
-
-
-function _get_secator_link($row,$params){
-	$link = 'index.php?option=com_content';
-
-	$Itemid = $params->get( 'Itemid', null );
-	$use_to = $params->get( 'use_to', 0 );
-
-	if($use_to==1){
-		// используем категроии
-		$task = $params->get( 'task_category', 'blogcategory' );
-	}else{
-		$task = $params->get( 'task_category', 'blogsection' );
-	}
-
-	$link = $link.'&task='.$task.'&id='.$row->id.'&Itemid='.$Itemid;
-
-	return sefRelToAbs($link);
+	mod_secator::use_sections($params); // используем разделы
 }
 
 ?>
