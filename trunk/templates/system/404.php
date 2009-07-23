bab56666c11c5ad74f9d67454c960266cf51036a
bost56 <bost56@3b7f9ca5-0b46-0410-b0b7-7d73c3805494>
2009-07-23 20:44:39 +0000

    Небольшое исправление локализации и обновление картинки версии

--- trunk/templates/system/404.php
+++ trunk/templates/system/404.php
@@ -14,7 +14,7 @@ if($mosConfig_lang == '') {
 	$mosConfig_lang = 'russian';
 }
 // load language file
-include_once ('language/'.$mosConfig_lang.'.php');
+include_once ('language/'.$mosConfig_lang.'/system.php');
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
