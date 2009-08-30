00c0b80fea12c1e1ecea6b6fd461385bfc20f24a
megazaisl <megazaisl@3b7f9ca5-0b46-0410-b0b7-7d73c3805494>
2009-08-30 04:56:36 +0000

    Шаблон NewLine версия 2.0

---
+++ templates/newline/js/template.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".content table tr:odd").addClass("tr-second");
+    $('div.moduletable-round').corner();
+});
+DD_roundies.addRule('.inputbox', '4px', true);
+DD_roundies.addRule('.dynamic-tab-pane-control', '5px', true);
+DD_roundies.addRule('h2.tab', '5px 5px 0px 0px', true);
+DD_roundies.addRule('.jstProfile_menu ul li a', '5px', true);
