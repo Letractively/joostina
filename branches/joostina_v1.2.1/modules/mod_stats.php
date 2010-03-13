4bc0ee182e8ea3cc533a5b30a8bc1e20aea23fd7
bost56 <bost56@3b7f9ca5-0b46-0410-b0b7-7d73c3805494>
2010-03-13 14:08:10 +0000

    Joostina v 1.2.1 a4

---
+++ branches/joostina_v1.2.1/modules/mod_stats.php
@@ -0,0 +1,56 @@
+<?php
+/**
+* @package Joostina
+* @copyright Авторские права (C) 2008 Joostina team. Все права защищены.
+* @license Лицензия http://www.gnu.org/licenses/gpl-2.0.htm GNU/GPL, или help/license.php
+* Joostina! - свободное программное обеспечение распространяемое по условиям лицензии GNU/GPL
+* Для получения информации о используемых расширениях и замечаний об авторском праве, смотрите файл help/copyright.php.
+*/
+// запрет прямого доступа
+defined('_VALID_MOS') or die();
+global $mosConfig_offset, $mosConfig_caching, $mosConfig_enable_stats;
+global $mosConfig_gzip;
+$serverinfo = $params->get('serverinfo');
+$siteinfo= $params->get('siteinfo');
+$content = '';
+if ($serverinfo) {
+echo "<strong>ОС:</strong>" .substr(php_uname(),0,7)."<br />\n";
+echo "<strong>PHP:</strong>" .phpversion()."<br />\n";
+echo "<strong>MySQL:</strong>" .$database->getVersion()."<br />\n";
+echo "<strong>"._TIME_STAT.":</strong>" .date("H:i",time()+($mosConfig_offset*60*60))."<br />\n";
+$c = $mosConfig_caching ? 'Разрешено':'Запрещено';
+echo "<strong>Кэширование:</strong>" .$c."<br />\n";
+$z = $mosConfig_gzip ? 'Разрешено':'Запрещено';
+echo "<strong>GZIP:</strong>" .$z."<br />\n";
+}
+if ($siteinfo) {
+$query="SELECT COUNT( id ) AS count_users FROM #__users";
+$database->setQuery($query);
+echo "<strong>"._MEMBERS_STAT.":</strong>" .$database->loadResult()."<br />\n";
+$query="SELECT COUNT( id ) AS count_items FROM #__content";
+$database->setQuery($query);
+echo "<strong>"._NEWS_STAT.":</strong>" .$database->loadResult()."<br />\n";
+$query="SELECT COUNT( id ) AS count_links"
+. "\n FROM #__weblinks"
+. "\n WHERE published = 1";
+$database->setQuery($query);
+echo "<strong>"._LINKS_STAT.":</strong>" .$database->loadResult()."<br />\n";
+}
+if ($mosConfig_enable_stats) {
+$counter= $params->get( 'counter' );
+$increase= $params->get( 'increase' );
+if ($counter) {
+$query = "SELECT SUM( hits ) AS count"
+. "\n FROM #__stats_agents"
+. "\n WHERE type = 1";
+$database->setQuery( $query );
+$hits = $database->loadResult();
+$hits = $hits + $increase;
+if ($hits == NULL) {
+$content .= "<strong>"._VISITORS.":</strong> 0\n";
+} else {
+$content .= "<strong>"._VISITORS.":</strong>" .$hits."\n";
+}
+}
+}
+?>
