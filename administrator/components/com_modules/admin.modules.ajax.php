3a1ae0b9ac31b827f063e53b346f4d5f7f9c2ef5
bost56 <bost56@3b7f9ca5-0b46-0410-b0b7-7d73c3805494>
2010-03-18 21:52:05 +0000

    Убрано использование устаревшей функции joostina_api::convert()

--- administrator/components/com_modules/admin.modules.ajax.php
+++ administrator/components/com_modules/admin.modules.ajax.php
@@ -61,11 +61,6 @@ function x_apply() {
 
 	$row = new mosModule($database);
 
-	$_POST['title'] = joostina_api::convert($_POST['title']);
-	if (isset($_POST['content'])) {
-		$_POST['content'] = joostina_api::convert(strval($_POST['content']));
-	}
-
 	if(!$row->bind($_POST,'selections')) return 'error-bind';
 	if(!$row->check()) return 'error-check';
 	if(!$row->store()) return 'error-store';
