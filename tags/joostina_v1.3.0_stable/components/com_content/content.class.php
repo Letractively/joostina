c15b8a49ab75aa1b1fa70e442dca7c8971397d66
bost56 <bost56@3b7f9ca5-0b46-0410-b0b7-7d73c3805494>
2010-12-09 21:59:20 +0000

    Исправление ошибки вывода объектов главной страницы

--- tags/joostina_v1.3.0_stable/components/com_content/content.class.php
+++ tags/joostina_v1.3.0_stable/components/com_content/content.class.php
@@ -1483,7 +1483,7 @@ class contentSqlHelper {
 				} elseif ($type == 2) {
 					$where[] = '( a.catid=' . implode(' OR a.catid=', $ids) . ' )';
 				}
-			} else {
+			} elseif( $id>0 ) {
 				if ($type == 1) {
 					$where[] = '( a.sectionid=' . $id. ' )';
 				} elseif ($type == 2) {
