8f81df0a01244fb3017ed882d7815dd5b876589a
bost56 <bost56@3b7f9ca5-0b46-0410-b0b7-7d73c3805494>
2011-06-25 20:15:52 +0000

    Исправлено что бы в результатах поиска (раздел/категория) брались как названия разделов/категорий из параметра title, а не name

--- components/com_search/search.html.php
+++ components/com_search/search.html.php
@@ -6,9 +6,9 @@
  * Joostina! - свободное программное обеспечение распространяемое по условиям лицензии GNU/GPL
  * Для получения информации о используемых расширениях и замечаний об авторском праве, смотрите файл help/copyright.php.
  */
-
 // запрет прямого доступа
 defined('_VALID_MOS') or die();
+
 /**
  * @package Joostina
  * @subpackage Search
@@ -16,7 +16,7 @@ defined('_VALID_MOS') or die();
 class search_html {
 
 	public static function openhtml($params) {
-		if($params->get('page_title')) {
+		if ($params->get('page_title')) {
 			?><div class="componentheading<?php echo $params->get('pageclass_sfx'); ?>"><h1><?php echo $params->get('header'); ?></h1></div><?php
 		}
 	}
@@ -24,32 +24,32 @@ class search_html {
 	public static function searchbox($searchword, &$lists, $params) {
 		global $Itemid, $mainframe;
 		?>
-<br />
+		<br />
 
-<form action="index.php" method="get" name="searchForm" id="searchForm">
-	<input type="hidden" name="option" value="com_search" />
-	<input type="hidden" name="Itemid" value="<?php echo $Itemid; ?>" />
-	<div class="contentpaneopen<?php echo $params->get('pageclass_sfx'); ?>">
-		<label for="search_searchword"><?php echo _PROMPT_KEYWORD; ?>:</label>
-		<br />
-		<input type="text" name="searchword" id="search_searchword" size="30" maxlength="20" value="<?php echo stripslashes($searchword);?>" class="inputbox" />
-		<span class="button"><input type="submit" name="submit2" value="<?php echo _SEARCH; ?>" class="button" /></span>
-		<br />
+		<form action="index.php" method="get" name="searchForm" id="searchForm">
+			<input type="hidden" name="option" value="com_search" />
+			<input type="hidden" name="Itemid" value="<?php echo $Itemid; ?>" />
+			<div class="contentpaneopen<?php echo $params->get('pageclass_sfx'); ?>">
+				<label for="search_searchword"><?php echo _PROMPT_KEYWORD; ?>:</label>
+				<br />
+				<input type="text" name="searchword" id="search_searchword" size="30" maxlength="20" value="<?php echo stripslashes($searchword); ?>" class="inputbox" />
+				<span class="button"><input type="submit" name="submit2" value="<?php echo _SEARCH; ?>" class="button" /></span>
+				<br />
 				<?php echo $lists['searchphrase']; ?>
-		<br />
-		<br />
-		<h3><?php echo _SEARCH_RESULTS ?></h3>
-		<label for="search_ordering"><?php echo _ORDERING; ?>:</label>
+				<br />
+				<br />
+				<h3><?php echo _SEARCH_RESULTS ?></h3>
+				<label for="search_ordering"><?php echo _ORDERING; ?>:</label>
 				<?php echo $lists['ordering']; ?>
-	</div>
-</form>
+			</div>
+		</form>
 		<?php
 	}
 
 	public static function searchintro($searchword, $params) {
 		?>
-<div class="searchintro<?php echo $params->get('pageclass_sfx'); ?>">
-	<h4><?php echo _PROMPT_KEYWORD , ' <span>' , stripslashes($searchword) , '</span>'; ?></h4>
+		<div class="searchintro<?php echo $params->get('pageclass_sfx'); ?>">
+			<h4><?php echo _PROMPT_KEYWORD, ' <span>', stripslashes($searchword), '</span>'; ?></h4>
 			<?php
 		}
 
@@ -77,13 +77,13 @@ class search_html {
 			$searchword = urldecode($searchword);
 			$searchword = htmlspecialchars($searchword, ENT_QUOTES, 'UTF-8');
 			?>
-</div>
-<br />
+		</div>
+		<br />
 		<?php
 		echo $pageNav->writePagesCounter();
 		$ordering = strtolower(strval(mosGetParam($_REQUEST, 'ordering', 'newest')));
-		$ordering_exist = array( 'newest','oldest','popular','alpha','category' );
-		$ordering = isset( $ordering_exist[$ordering] ) ? $ordering : 'newest';
+		$ordering_exist = array('newest', 'oldest', 'popular', 'alpha', 'category');
+		$ordering = isset($ordering_exist[$ordering]) ? $ordering : 'newest';
 		$searchphrase = strtolower(strval(mosGetParam($_REQUEST, 'searchphrase', 'any')));
 		$searchphrase = htmlspecialchars($searchphrase, ENT_QUOTES, 'UTF-8');
 		$cleanWord = htmlspecialchars($searchword, ENT_QUOTES, 'UTF-8');
@@ -92,128 +92,132 @@ class search_html {
 		echo $pageNav->getLimitBox($link);
 		//}
 		?>
-<br /><br />
-<table class="contentpaneopen<?php echo $params->get('pageclass_sfx'); ?>">
-	<tr class="<?php echo $params->get('pageclass_sfx'); ?>">
-		<td><h4><?php eval('echo "' . _CONCLUSION . '";'); ?></h4>
+		<br /><br />
+		<table class="contentpaneopen<?php echo $params->get('pageclass_sfx'); ?>">
+			<tr class="<?php echo $params->get('pageclass_sfx'); ?>">
+				<td><h4><?php eval('echo "' . _CONCLUSION . '";'); ?></h4>
 					<?php
 					$z = $limitstart + 1;
 					$end = $limit + $z;
-					if($end > $total) {
+					if ($end > $total) {
 						$end = $total + 1;
 					}
-					for($i = $z; $i < $end; $i++) {
+					for ($i = $z; $i < $end; $i++) {
 						$row = $rows[$i - 1];
-						if($row->created) {
+						if ($row->created) {
 							$created = mosFormatDate($row->created, _DATE_FORMAT_LC);
 						} else {
 							$created = '';
 						}
 						?>
-			<fieldset>
-				<div>
-					<span class="small<?php echo $params->get('pageclass_sfx'); ?>"><?php echo $i . '. '; ?></span>
+						<fieldset>
+							<div>
+								<span class="small<?php echo $params->get('pageclass_sfx'); ?>"><?php echo $i . '. '; ?></span>
 								<?php
-								if($row->href) {
+								if ($row->href) {
 									$row->href = ampReplace($row->href);
-									if($row->browsernav == 1) {
+									if ($row->browsernav == 1) {
 										?>
-					<a href="<?php echo sefRelToAbs($row->href); ?>" target="_blank">
+										<a href="<?php echo sefRelToAbs($row->href); ?>" target="_blank">
 											<?php
 										} else {
 											?>
-						<a href="<?php echo sefRelToAbs($row->href); ?>">
+											<a href="<?php echo sefRelToAbs($row->href); ?>">
 												<?php
 											}
 										}
 										echo $row->title;
-										if($row->href) {
+										if ($row->href) {
 											?>
-						</a>
+										</a>
 										<?php
 									}
-									if($row->section) {
+									if ($row->section) {
 										?>
-						<br />
-						<span class="small<?php echo $params->get('pageclass_sfx'); ?>">(<?php echo $row->section; ?>)</span>
+										<br />
+										<span class="small<?php echo $params->get('pageclass_sfx'); ?>">(<?php echo $row->section; ?>)</span>
 										<?php
 									}
 									?>
-				</div>
-				<div><?php echo ampReplace($row->text); ?></div>
+							</div>
+							<div><?php echo ampReplace($row->text); ?></div>
 							<?php
-							if($mosConfig_showCreateDate) {
+							if ($mosConfig_showCreateDate) {
 								?>
-				<div class="small<?php echo $params->get('pageclass_sfx'); ?>"><?php echo $created; ?></div>
+								<div class="small<?php echo $params->get('pageclass_sfx'); ?>"><?php echo $created; ?></div>
 								<?php
 							}
 							?>
-			</fieldset>
-			<br />
+						</fieldset>
+						<br />
 						<?php
 					}
 					?>
-		</td>
-	</tr>
-</table>
-<br />
-<nofollow>
-<a href="http://sm.aport.ru/search?That=std&r=<?php echo $searchword; ?>" target="_blank"><?php echo $image; ?></a>
-<a href="http://www.bing.com/search?q=<?php echo $searchword; ?>" target="_blank"><?php echo $image1; ?></a>
-<a href="http://gogo.ru/go?q=<?php echo $searchword; ?>" target="_blank"><?php echo $image2; ?></a>
-<a href="http://www.google.ru/webhp#hl=ru&q=<?php echo $searchword; ?>" target="_blank"><?php echo $image3; ?></a>
-<a href="http://go.mail.ru/search?q=<?php echo $searchword; ?>" target="_blank"><?php echo $image4; ?></a>
-<a href="http://www.nigma.ru/index.php?s=<?php echo $searchword; ?>" target="_blank"><?php echo $image5; ?></a>
-<a href="http://nova.rambler.ru/srch?words=<?php echo $searchword; ?>" target="_blank"><?php echo $image6; ?></a>
-<a href="http://ru.search.yahoo.com/search?p=<?php echo $searchword; ?>" target="_blank"><?php echo $image7; ?></a>
-<a href="http://yandex.ru/yandsearch?text=<?php echo $searchword; ?>" target="_blank"><?php echo $image8; ?></a>
-</nofollow>
-<br />
+				</td>
+			</tr>
+		</table>
+		<br />
+		<nofollow>
+			<a href="http://sm.aport.ru/search?That=std&r=<?php echo $searchword; ?>" target="_blank"><?php echo $image; ?></a>
+			<a href="http://www.bing.com/search?q=<?php echo $searchword; ?>" target="_blank"><?php echo $image1; ?></a>
+			<a href="http://gogo.ru/go?q=<?php echo $searchword; ?>" target="_blank"><?php echo $image2; ?></a>
+			<a href="http://www.google.ru/webhp#hl=ru&q=<?php echo $searchword; ?>" target="_blank"><?php echo $image3; ?></a>
+			<a href="http://go.mail.ru/search?q=<?php echo $searchword; ?>" target="_blank"><?php echo $image4; ?></a>
+			<a href="http://www.nigma.ru/index.php?s=<?php echo $searchword; ?>" target="_blank"><?php echo $image5; ?></a>
+			<a href="http://nova.rambler.ru/srch?words=<?php echo $searchword; ?>" target="_blank"><?php echo $image6; ?></a>
+			<a href="http://ru.search.yahoo.com/search?p=<?php echo $searchword; ?>" target="_blank"><?php echo $image7; ?></a>
+			<a href="http://yandex.ru/yandsearch?text=<?php echo $searchword; ?>" target="_blank"><?php echo $image8; ?></a>
+		</nofollow>
+		<br />
 		<?php
 	}
 
 	public static function conclusion($searchword, $pageNav) {
 		global $option, $Itemid;
 		$ordering = strtolower(strval(mosGetParam($_REQUEST, 'ordering', 'newest')));
-		$ordering_exist = array( 'newest','oldest','popular','alpha','category' );
-		$ordering = isset( $ordering_exist[$ordering] ) ? $ordering : 'newest';
+		$ordering_exist = array('newest', 'oldest', 'popular', 'alpha', 'category');
+		$ordering = isset($ordering_exist[$ordering]) ? $ordering : 'newest';
 		$searchphrase = strtolower(strval(mosGetParam($_REQUEST, 'searchphrase', 'any')));
 		$searchphrase = htmlspecialchars($searchphrase);
 		$link = JPATH_SITE . "/index.php?option=$option&amp;Itemid=$Itemid&amp;searchword=$searchword&amp;searchphrase=$searchphrase&amp;ordering=$ordering";
 		echo $pageNav->writePagesLinks($link);
 	}
+
 }
 
 class search_by_tag_HTML {
 
-	public static function tag_page ($items, $params, $groups) {
+	public static function tag_page($items, $params, $groups) {
 		?>
-<div class="tag_page">
-	<div class="contentpagetitle">
-		<h1><?php echo $params->title;?> "<?php echo $items->tag;?>"</h1>
-		<div class="search_result"><?php echo self::view_group($items, $params, $groups);?></div>
-	</div>
-</div>
+		<div class="tag_page">
+			<div class="contentpagetitle">
+				<h1><?php echo $params->title; ?> "<?php echo $items->tag; ?>"</h1>
+				<div class="search_result"><?php echo self::view_group($items, $params, $groups); ?></div>
+			</div>
+		</div>
 		<?php
 	}
 
 	public static function view_group($items, $params, $groups) {
-		if(count($items->items['com_content'])>0) {
-			foreach($groups as $key=>$group) {
-				foreach($items->items[$key] as $item) {
+		if (count($items->items['com_content']) > 0) {
+			foreach ($groups as $key => $group) {
+				foreach ($items->items[$key] as $item) {
 					$item->link = searchByTag::construct_url($item, $group);
 					$item->text = Text::word_limiter(mosHTML::cleanText($item->text), 25);
 					?><div class="search_item">
-	<h2><a class="contentpagetitle" href="<?php echo $item->link;?>"><?php echo $item->title;?></a> </h2>
-	<span class="date"><?php echo $item->date;?></span> <br />
-	<p><?php echo $item->text;?></p>
-</div>
-					<?php }
+						<h2><a class="contentpagetitle" href="<?php echo $item->link; ?>"><?php echo $item->title; ?></a> </h2>
+						<span class="date"><?php echo $item->date; ?></span> <br />
+						<p><?php echo $item->text; ?></p>
+					</div>
+				<?php
+				}
 			}
-		}else {?>
-<div><?php echo _SEARCH_NONE_W_TAG?></div>
-			<?php };?>
+		} else {
+			?>
+			<div><?php echo _SEARCH_NONE_W_TAG ?></div>
+		<?php }; ?>
 		<?php
 	}
+
 }
 ?>
--- mambots/search/content.searchbot.php
+++ mambots/search/content.searchbot.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package Joostina
  * @copyright Авторские права (C) 2008-2010 Joostina team. Все права защищены.
@@ -6,11 +7,10 @@
  * Joostina! - свободное программное обеспечение распространяемое по условиям лицензии GNU/GPL
  * Для получения информации о используемых расширениях и замечаний об авторском праве, смотрите файл help/copyright.php.
  */
-
 // запрет прямого доступа
 defined('_VALID_MOS') or die();
 
-$_MAMBOTS->registerFunction('onSearch','botSearchContent');
+$_MAMBOTS->registerFunction('onSearch', 'botSearchContent');
 
 /**
  * Content Search method
@@ -21,13 +21,13 @@ $_MAMBOTS->registerFunction('onSearch','botSearchContent');
  * @param сопоставляет параметры: exact|any|all
  * @param определяет параметр сортировки: newest|oldest|popular|alpha|category
  */
-function botSearchContent($text,$phrase = '',$ordering = '') {
-	global $my,$_MAMBOTS;
+function botSearchContent($text, $phrase = '', $ordering = '') {
+	global $my, $_MAMBOTS;
 
 	$database = database::getInstance();
 
 	// check if param query has previously been processed
-	if(!isset($_MAMBOTS->_search_mambot_params['content'])) {
+	if (!isset($_MAMBOTS->_search_mambot_params['content'])) {
 		// load mambot params info
 		$query = "SELECT params FROM #__mambots WHERE element = 'content.searchbot' AND folder = 'search'";
 		$database->setQuery($query);
@@ -42,19 +42,19 @@ function botSearchContent($text,$phrase = '',$ordering = '') {
 
 	$botParams = new mosParameters($mambot->params);
 
-	$limit = $botParams->def('search_limit',50);
-	$nonmenu = $botParams->def('nonmenu',1);
+	$limit = $botParams->def('search_limit', 50);
+	$nonmenu = $botParams->def('nonmenu', 1);
 
 	$nullDate = $database->getNullDate();
 	$now = _CURRENT_SERVER_TIME;
 
 	$text = trim($text);
-	if($text == '') {
+	if ($text == '') {
 		return array();
 	}
 
 	$wheres = array();
-	switch($phrase) {
+	switch ($phrase) {
 		case 'exact':
 			$wheres2 = array();
 			$wheres2[] = "LOWER(a.title) LIKE LOWER('%$text%')";
@@ -62,29 +62,29 @@ function botSearchContent($text,$phrase = '',$ordering = '') {
 			$wheres2[] = "LOWER(a.fulltext) LIKE LOWER('%$text%')";
 			$wheres2[] = "LOWER(a.metakey) LIKE LOWER('%$text%')";
 			$wheres2[] = "LOWER(a.metadesc) LIKE LOWER('%$text%')";
-			$where = '('.implode(') OR (',$wheres2).')';
+			$where = '(' . implode(') OR (', $wheres2) . ')';
 			break;
 
 		case 'all':
 		case 'any':
 		default:
-			$words = explode(' ',$text);
+			$words = explode(' ', $text);
 			$wheres = array();
-			foreach($words as $word) {
+			foreach ($words as $word) {
 				$wheres2 = array();
 				$wheres2[] = "LOWER(a.title) LIKE LOWER('%$word%')";
 				$wheres2[] = "LOWER(a.introtext) LIKE LOWER('%$word%')";
 				$wheres2[] = "LOWER(a.fulltext) LIKE LOWER('%$word%')";
 				$wheres2[] = "LOWER(a.metakey) LIKE LOWER('%$word%')";
 				$wheres2[] = "LOWER(a.metadesc) LIKE LOWER('%$word%')";
-				$wheres[] = implode(' OR ',$wheres2);
+				$wheres[] = implode(' OR ', $wheres2);
 			}
-			$where = '('.implode(($phrase == 'all'?') AND (':') OR ('),$wheres).')';
+			$where = '(' . implode(($phrase == 'all' ? ') AND (' : ') OR ('), $wheres) . ')';
 			break;
 	}
 
 	$morder = '';
-	switch($ordering) {
+	switch ($ordering) {
 		case 'oldest':
 			$order = 'a.created ASC';
 			break;
@@ -109,71 +109,71 @@ function botSearchContent($text,$phrase = '',$ordering = '') {
 	}
 
 	// search content items
-	$query = "SELECT a.title AS title,"."\n a.created AS created,"."\n CONCAT(a.introtext, a.fulltext) AS text,".
-			"\n CONCAT_WS( '/', u.name, b.name ) AS section,"."\n CONCAT( 'index.php?option=com_content&task=view&id=', a.id ) AS href,".
-			"\n '2' AS browsernav,"."\n 'content' AS type"."\n, u.id AS sec_id, b.id as cat_id".
-			"\n FROM #__content AS a"."\n INNER JOIN #__categories AS b ON b.id=a.catid"."\n INNER JOIN #__sections AS u ON u.id = a.sectionid".
-			"\n WHERE ( $where )"."\n AND a.state = 1"."\n AND u.published = 1"."\n AND b.published = 1".
-			"\n AND a.access <= ".(int)$my->gid."\n AND b.access <= ".(int)$my->gid."\n AND u.access <= ".(int)
-			$my->gid."\n AND ( a.publish_up = ".$database->Quote($nullDate).
-			" OR a.publish_up <= ".$database->Quote($now)." )"."\n AND ( a.publish_down = ".
-			$database->Quote($nullDate)." OR a.publish_down >= ".$database->Quote($now).
-			" )"."\n GROUP BY a.id"."\n ORDER BY $order";
-	$database->setQuery($query,0,$limit);
+	$query = "SELECT a.title AS title," . "\n a.created AS created," . "\n CONCAT(a.introtext, a.fulltext) AS text," .
+			"\n CONCAT_WS( '/', u.title, b.title ) AS section," . "\n CONCAT( 'index.php?option=com_content&task=view&id=', a.id ) AS href," .
+			"\n '2' AS browsernav," . "\n 'content' AS type" . "\n, u.id AS sec_id, b.id as cat_id" .
+			"\n FROM #__content AS a" . "\n INNER JOIN #__categories AS b ON b.id=a.catid" . "\n INNER JOIN #__sections AS u ON u.id = a.sectionid" .
+			"\n WHERE ( $where )" . "\n AND a.state = 1" . "\n AND u.published = 1" . "\n AND b.published = 1" .
+			"\n AND a.access <= " . (int) $my->gid . "\n AND b.access <= " . (int) $my->gid . "\n AND u.access <= " . (int)
+			$my->gid . "\n AND ( a.publish_up = " . $database->Quote($nullDate) .
+			" OR a.publish_up <= " . $database->Quote($now) . " )" . "\n AND ( a.publish_down = " .
+			$database->Quote($nullDate) . " OR a.publish_down >= " . $database->Quote($now) .
+			" )" . "\n GROUP BY a.id" . "\n ORDER BY $order";
+	$database->setQuery($query, 0, $limit);
 	$list = $database->loadObjectList();
 
 	// search static content
-	$query = "SELECT a.title AS title,"."\n a.created AS created,"."\n a.introtext AS text,".
-			"\n ".$database->Quote(_STATIC_CONTENT)." AS section,"."\n CONCAT( 'index.php?option=com_content&task=view&id=', a.id, '&Itemid=', m.id ) AS href,".
-			"\n '2' AS browsernav,"."\n a.id"."\n FROM #__content AS a"."\n LEFT JOIN #__menu AS m ON m.componentid = a.id".
-			"\n WHERE ($where)"."\n AND a.state = 1"."\n AND a.access <= ".(int)$my->gid."\n AND m.type = 'content_typed'".
-			"\n AND ( a.publish_up = ".$database->Quote($nullDate)." OR a.publish_up <= ".$database->Quote($now).
-			" )"."\n AND ( a.publish_down = ".$database->Quote($nullDate).
-			" OR a.publish_down >= ".$database->Quote($now)." )"."\n GROUP BY a.id"."\n ORDER BY ".($morder?
-			$morder:$order);
-	$database->setQuery($query,0,$limit);
+	$query = "SELECT a.title AS title," . "\n a.created AS created," . "\n a.introtext AS text," .
+			"\n " . $database->Quote(_STATIC_CONTENT) . " AS section," . "\n CONCAT( 'index.php?option=com_content&task=view&id=', a.id, '&Itemid=', m.id ) AS href," .
+			"\n '2' AS browsernav," . "\n a.id" . "\n FROM #__content AS a" . "\n LEFT JOIN #__menu AS m ON m.componentid = a.id" .
+			"\n WHERE ($where)" . "\n AND a.state = 1" . "\n AND a.access <= " . (int) $my->gid . "\n AND m.type = 'content_typed'" .
+			"\n AND ( a.publish_up = " . $database->Quote($nullDate) . " OR a.publish_up <= " . $database->Quote($now) .
+			" )" . "\n AND ( a.publish_down = " . $database->Quote($nullDate) .
+			" OR a.publish_down >= " . $database->Quote($now) . " )" . "\n GROUP BY a.id" . "\n ORDER BY " . ($morder ?
+					$morder : $order);
+	$database->setQuery($query, 0, $limit);
 	$list2 = $database->loadObjectList();
 
 	// поиск архивного содержимого
-	$query = "SELECT a.title AS title,"."\n a.created AS created,"."\n a.introtext AS text,".
-			"\n CONCAT_WS( '/', ".$database->Quote(_IN_ARCHIVE).
-			", u.name, b.name ) AS section,"."\n CONCAT('index.php?option=com_content&task=view&id=',a.id) AS href,".
-			"\n '2' AS browsernav,"."\n 'content' AS type"."\n FROM #__content AS a"."\n INNER JOIN #__categories AS b ON b.id=a.catid".
-			"\n INNER JOIN #__sections AS u ON u.id = a.sectionid"."\n WHERE ( $where )"."\n AND a.state = -1".
-			"\n AND u.published = 1"."\n AND b.published = 1"."\n AND a.access <= ".(int)$my->gid.
-			"\n AND b.access <= ".(int)$my->gid."\n AND u.access <= ".(int)$my->gid."\n AND ( a.publish_up = ".
-			$database->Quote($nullDate)." OR a.publish_up <= ".$database->Quote($now)." )".
-			"\n AND ( a.publish_down = ".$database->Quote($nullDate).
-			" OR a.publish_down >= ".$database->Quote($now)." )"."\n ORDER BY $order";
-	$database->setQuery($query,0,$limit);
+	$query = "SELECT a.title AS title," . "\n a.created AS created," . "\n a.introtext AS text," .
+			"\n CONCAT_WS( '/', " . $database->Quote(_IN_ARCHIVE) .
+			", u.title, b.title ) AS section," . "\n CONCAT('index.php?option=com_content&task=view&id=',a.id) AS href," .
+			"\n '2' AS browsernav," . "\n 'content' AS type" . "\n FROM #__content AS a" . "\n INNER JOIN #__categories AS b ON b.id=a.catid" .
+			"\n INNER JOIN #__sections AS u ON u.id = a.sectionid" . "\n WHERE ( $where )" . "\n AND a.state = -1" .
+			"\n AND u.published = 1" . "\n AND b.published = 1" . "\n AND a.access <= " . (int) $my->gid .
+			"\n AND b.access <= " . (int) $my->gid . "\n AND u.access <= " . (int) $my->gid . "\n AND ( a.publish_up = " .
+			$database->Quote($nullDate) . " OR a.publish_up <= " . $database->Quote($now) . " )" .
+			"\n AND ( a.publish_down = " . $database->Quote($nullDate) .
+			" OR a.publish_down >= " . $database->Quote($now) . " )" . "\n ORDER BY $order";
+	$database->setQuery($query, 0, $limit);
 	$list3 = $database->loadObjectList();
 
 	// check if search of nonmenu linked static content is allowed
-	if($nonmenu) {
+	if ($nonmenu) {
 		// collect ids of static content items linked to menu items
 		// so they can be removed from query that follows
 		$ids = null;
-		if(count($list2)) {
-			foreach($list2 as $static) {
-				$ids[] = (int)$static->id;
+		if (count($list2)) {
+			foreach ($list2 as $static) {
+				$ids[] = (int) $static->id;
 			}
-			$ids = "a.id != ".implode(" OR a.id != ",$ids);
+			$ids = "a.id != " . implode(" OR a.id != ", $ids);
 		}
 
 		// search static content not connected to a menu
-		$query = "SELECT a.title AS title,"."\n a.created AS created,"."\n a.introtext AS text,".
-				"\n '2' as browsernav, ".$database->Quote(_STATIC_CONTENT)." AS section,"."\n CONCAT( 'index.php?option=com_content&task=view&id=', a.id ) AS href,".
-				"\n a.id"."\n FROM #__content AS a"."\n WHERE ($where)".(($ids)?"\n AND ( $ids )":
-				'')."\n AND a.state = 1"."\n AND a.access <= ".(int)$my->gid."\n AND a.sectionid = 0".
-				"\n AND ( a.publish_up = ".$database->Quote($nullDate)." OR a.publish_up <= ".$database->Quote($now).
-				" )"."\n AND ( a.publish_down = ".$database->Quote($nullDate).
-				" OR a.publish_down >= ".$database->Quote($now)." )"."\n ORDER BY ".($morder?$morder:
-				$order);
-		$database->setQuery($query,0,$limit);
+		$query = "SELECT a.title AS title," . "\n a.created AS created," . "\n a.introtext AS text," .
+				"\n '2' as browsernav, " . $database->Quote(_STATIC_CONTENT) . " AS section," . "\n CONCAT( 'index.php?option=com_content&task=view&id=', a.id ) AS href," .
+				"\n a.id" . "\n FROM #__content AS a" . "\n WHERE ($where)" . (($ids) ? "\n AND ( $ids )" :
+						'') . "\n AND a.state = 1" . "\n AND a.access <= " . (int) $my->gid . "\n AND a.sectionid = 0" .
+				"\n AND ( a.publish_up = " . $database->Quote($nullDate) . " OR a.publish_up <= " . $database->Quote($now) .
+				" )" . "\n AND ( a.publish_down = " . $database->Quote($nullDate) .
+				" OR a.publish_down >= " . $database->Quote($now) . " )" . "\n ORDER BY " . ($morder ? $morder :
+						$order);
+		$database->setQuery($query, 0, $limit);
 		$list4 = $database->loadObjectList();
 	} else {
 		$list4 = array();
 	}
 
-	return array_merge($list,$list2,$list3,(array )$list4);
+	return array_merge($list, $list2, $list3, (array) $list4);
 }
